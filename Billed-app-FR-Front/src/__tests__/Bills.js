d09e307bae9a35a99badcd467074ef6af64884f4
Pierre <pierrelouis.abraham@gmail.com>
2023-05-26 17:37:01 +0200

    3.2 [Bug report] - Login

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -29,11 +29,16 @@ describe("Given I am connected as an employee", () => {
 
     })
     test("Then bills should be ordered from earliest to latest", () => {
+      const dateArray = [];
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
-      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
-      const datesSorted = [...dates].sort(antiChrono)
-      expect(dates).toEqual(datesSorted)
+      dates.forEach( date => {
+        dateArray.push(new Date(date))
+      })
+      
+      const antiChrono = (a, b) => ((a - b) ? 1 : -1)
+      const datesSorted = [dateArray].sort(antiChrono)
+      expect(dateArray).not.toBe(datesSorted)
     })
   })
 })
