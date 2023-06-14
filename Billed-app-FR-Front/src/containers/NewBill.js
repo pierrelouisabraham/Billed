24dd1f5edb025cd241137c46410b13d1316a68b9
Pierre <pierrelouis.abraham@gmail.com>
2023-06-14 18:47:19 +0200

    test

--- Billed-app-FR-Front/src/containers/NewBill.js
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -13,81 +13,93 @@ export default class NewBill {
     this.fileUrl = null
     this.fileName = null
     this.billId = null
+    this.isValidExtension = false
     new Logout({ document, localStorage, onNavigate })
   }
+
+  hasValidExtension = (extension, validExtensions = ['jpg', 'jpeg', 'png']) => validExtensions.includes(extension.toLowerCase())
+
   handleChangeFile = e => {
     e.preventDefault()
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
-    const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length-1]
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
+
+    // Check if file name contains extension valid
+    const fileNameSplitted = file.name.split('.')
+    const extension = fileNameSplitted[fileNameSplitted.length - 1].toLowerCase()
+
+    // let regex = new RegExp(/[^\s]+(.*?).(jpg|jpeg|png|gif|JPG|JPEG|PNG|GIF)$/);
+    console.log(extension)
+    console.log(this.hasValidExtension(extension))
+    if (this.hasValidExtension(extension)) {
+      this.isValidExtension = true
+      this.createFile(formData)
+      console.log('extension valide')
+    } else {
+      console.log('extension invalide')
+      alert('Extension invalide')
+      this.isValidExtension = false
+    }
+
     formData.append('file', file)
     formData.append('email', email)
-    let regex = new RegExp(/[^\s]+(.*?).(jpg|jpeg|png|gif|JPG|JPEG|PNG|GIF)$/);
-    if (regex.test(fileName) == true) {
-      this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          noContentType: true
-        }
-      })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl, key, fileName)
-        this.billId = key
-        this.fileUrl = fileUrl
-        this.fileName = fileName
-      }).catch(error => console.error(error))
-  }
-  else {
-      return "false";
-  }
-    
-  }
-  handleSubmit = e => {
-    e.preventDefault()
-    const user = localStorage.getItem("user");
-  if (!user) {
-    console.log("User data not found in localStorage");
-    return;
+    this.fileName = file.name
   }
 
-  let email;
-  try {
-    email = JSON.parse(user).email;
-  } catch (error) {
-    console.log("Failed to parse user email from localStorage");
-    return;
-  }
-    const bill = {
-      email,
-      type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-      name:  e.target.querySelector(`input[data-testid="expense-name"]`).value,
-      amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
-      date:  e.target.querySelector(`input[data-testid="datepicker"]`).value,
-      vat: e.target.querySelector(`input[data-testid="vat"]`).value,
-      pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
-      commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
-      fileUrl: this.fileUrl,
-      fileName: this.fileName,
-      status: 'pending'
+  handleSubmit = e => {
+    e.preventDefault()
+    if (this.isValidExtension) {
+      const email = JSON.parse(localStorage.getItem("user")).email
+      const bill = {
+        email,
+        type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
+        name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
+        amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
+        date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
+        vat: e.target.querySelector(`input[data-testid="vat"]`).value,
+        pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
+        commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
+        fileUrl: this.fileUrl,
+        fileName: this.fileName,
+        status: 'pending'
+      }
+      this.updateBill(bill)
+      this.onNavigate(ROUTES_PATH['Bills'])
+    } else {  
+      alert('vous ne pouvez pas soumettre tant que vous avez un fichier invalide')
     }
-    this.updateBill(bill)
-    this.onNavigate(ROUTES_PATH['Bills'])
+  }
+
+  createFile = data => {
+    if (this.store) {
+      this.store
+        .bills()
+        .create({
+          data,
+          headers: {
+            noContentType: true
+          }
+        })
+        .then(({ fileUrl, key }) => {
+          console.log(fileUrl, key)
+          this.billId = key
+          this.fileUrl = fileUrl
+          this.fileName = fileName
+        }).catch(error => console.error(error))
+    }
   }
 
   // not need to cover this function by tests
   updateBill = (bill) => {
     if (this.store) {
       this.store
-      .bills()
-      .update({data: JSON.stringify(bill), selector: this.billId})
-      .then(() => {
-        this.onNavigate(ROUTES_PATH['Bills'])
-      })
-      .catch(error => console.error(error))
+        .bills()
+        .update({ data: JSON.stringify(bill), selector: this.billId })
+        .then(() => {
+          this.onNavigate(ROUTES_PATH['Bills'])
+        })
+        .catch(error => console.error(error))
     }
   }
 }
